46dc6e136757dfcc6043eddd82336a26e38d2b45
Adventure 2019 <caliew26@gmail.com>
2019-04-15 21:42:58 -0400

    added functionality for the game, works as designed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,45 @@
-// var topics = ["Mickey Mouse", "Minnie Mouse", "Donald Duck", "Daisy Duck","Huey Duck", "Dewey Duck", "Louie Duck", "Scrooge McDuck", "Goofy","Pluto", "Chip'n Dale", "Clarice", "Pete"];
+//setting global variable
+var topics = ["Mickey Mouse", "Minnie Mouse", "Donald Duck", "Daisy Duck","Huey Duck", "Dewey Duck", "Scrooge McDuck", "Goofy", "Pluto", "Peter Pan", "Cinderella", "Jasmine", "Sleeping Beauty", "Rapunzel", "Belle", "Pocahontas", "Walt Disney"];
 
-var topics = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+//var topics = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
 
-
+//getting the DOM loaded/rendered
 $(document).ready(function() {
+    //cycle through a for loop that will cycle through the number of items in the array
     for(let i = 0; i < topics.length; i++){
+        //will create a button for each iterration within the array
         createTopicButton(topics[i]);
     }
 });
     
+//create a function called createTopicButton and assign a parameter called newTopicText
 function createTopicButton(newTopicText){
+    //declare a variable and assign the element
     var btn = document.createElement("BUTTON");
+    //add a bootstrap class to the element
     $(btn).addClass("btn-sm");
+    //add a class to the element that will enable the eventhandler 
     $(btn).addClass("topicsButton");
+    //add text onto the new button
     $(btn).text(newTopicText);
+    //adding the button just generated to the DOM/HTML
     $("#topicsButtons").append(btn);
+    //calling the function 
     initializeEventHandlers();
 }
 
+//create a function
 function initializeEventHandlers(){
+    //need to remove all eventhandlers from the element id
     $("#newTopicSubmit").unbind("click");
+    //when the user inputs a value into the 
     $("#newTopicSubmit").click(function(){
         var newTopicInput = $("#newTopicInput").val();
         if(newButtonValid(newTopicInput)){
             createTopicButton(newTopicInput);
             topics.push(newTopicInput)
         } else {
-            alert("wrong")
+            alert("Please add a character that is not on the current list")
         };
     });
 
@@ -46,28 +59,47 @@ function newButtonValid (newTopicText){
 
 function getGiphyStuff(queryText){
     $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?api_key=NahfTXxPEBW0emPiN880sy1Vhm8llXUy&q=" + queryText + "&limit=10",
+        url: "https://api.giphy.com/v1/gifs/search?api_key=NahfTXxPEBW0emPiN880sy1Vhm8llXUy&q=" + queryText + "&limit=3",
         method: "GET"
     }).then(function(response){
         $("#placehere").empty();
         for(let i = 0; i < response.data.length; i++){
-        gifOntoPage(response.data[i].images.downsized_still.url,response.data[i].images.downsized.url);
-        // console.log(response.data[i].images.downsized.url)
+        gifOntoPage(response.data[i].images.downsized_still.url,response.data[i].images.downsized.url,response.data[i].rating);
+        console.log(response.data[i]);
     }
     });
 }
 
-function gifOntoPage(url,urlanime){
+function gifOntoPage(url,urlAnime,rating){
     var gifDiv = document.createElement("div");
     var gifPlaceHolder = document.createElement("img");
     gifPlaceHolder.setAttribute("src", url);
+    // gifPlaceHolder.setAttribute("onclick", "gifSwaper('" + urlAnime + "')");
+    $(gifPlaceHolder).click(function(){
+        gifSwaper(urlAnime,this,url);
+    });
+    // gifPlaceHolder.onclick = gifSwaper(urlAnime);
     // gifPlaceHolder.setAttribute("height", "768");
     gifPlaceHolder.setAttribute("width", "40%");
-    var gifLink = document.createElement("a")
-    gifLink.setAttribute("href", urlanime);
-    $(gifLink).append(gifPlaceHolder);
-    $(gifDiv).append(gifLink);
+    var gifRating = document.createElement("p");
+    $(gifRating).addClass("rating");
+    $(gifRating).text("Rating: " + rating);
+    // var gifLink = document.createElement("a")
+    // var gifAnime = document.createElement("a")
+    // gifLink.setAttribute("href", urlanime);
+    // gifAnime.setAttribute("href", urlStill);
+    // $(gifPlaceHolder).append(gifAnime);
+    $(gifDiv).append(rating);
+    // $(gifLink).append(gifPlaceHolder);
+    $(gifDiv).append(gifPlaceHolder);
+    // $(gifDiv).append(gifLink);
     $("#placehere").append(gifDiv);
 }
 
-//account for spaces within the newTopicText
+function gifSwaper(newUrl,image,url){
+    image.setAttribute("src", newUrl);
+    $(image).unbind("click");
+    $(image).click(function(){
+        gifSwaper(url,image,newUrl);
+    });
+};
